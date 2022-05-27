8410e15a7da3f60f3ca49b9b3f944732925f13a6
SinJi7 <93049777+SinJi7@users.noreply.github.com>
2022-05-27 22:25:31 +0900

    Update search.py

--- search.py
+++ search.py
@@ -60,7 +60,37 @@ def bfs(graph, start):
                 vist.append(i)
             
         if not dq : break
-    return 
+    return
+
+def dfs2(graph, start):
+    print("DFS2: ", end="")
+    visit = []
+    
+    dq = deque()
+    visit.append(start)
+    dq.append(start)
+    VisNode = start
+    
+    print(VisNode, end=", ")
+    
+    while True:
+        #다음 방문 노드, 탐색 성공여
+        findNode = False
+        #print("visNode: ", VisNode)
+        #print(visit)
+        
+        for i in graph[VisNode]:
+            #방문하지 않은 노드를 찾는다.
+            if i not in visit:
+                findNode = True 
+                
+                VisNode = i #다음 방문 노드
+                
+                visit.append(i) #방문한 노드에 i 추가 
+                dq.append(i) #스택에 i추가 
+                print(VisNode, end=", ")
+                #찾을시 반복문 종료
+                break
 
 my_graph = {
     1 : [2, 3],
